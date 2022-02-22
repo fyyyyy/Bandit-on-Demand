1bf6a9436c3a69b02dc4f43178e575f369300c63
fyyyyy <frankyyyy@live.com>
2022-02-22 19:11:32 +0100

    update batch files

--- compress.bat
+++ compress.bat
@@ -1,4 +1,9 @@
 
+@REM extract
+@REM powershell -Command "7z x -aoa 'C:\Users\frank\Saved Games\DCS\Missions\Bandit on Demand.miz' -o'.\Bandit on Demand\'"
+@REM powershell -Command "7z x -aoa 'C:\Users\frank\Saved Games\DCS\Missions\Bandit on Demand Syria.miz' -o'.\Bandit on Demand Syria\'"
+
+@REM compile and copy
 powershell -Command "Copy-Item -Force './scripts/spawn_ctrl.lua' -Destination '.\Bandit on Demand\l10n\DEFAULT\spawn_ctrl.lua'"
 powershell -Command "7z a -tzip BanditonDemand.zip '.\Bandit on Demand\*'"
 powershell -Command "Copy-Item -Force './BanditonDemand.zip' -Destination 'C:\Users\frank\Saved Games\DCS\Missions\Bandit on Demand.miz'"
